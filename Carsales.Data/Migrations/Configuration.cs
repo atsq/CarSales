2c513167196a3a2fc175f38cd33d3cd6f01e7c07
atsq <atsq17@gmail.com>
2016-04-26 16:02:54 +1000

    Set up seed data

--- Carsales.Data/Migrations/Configuration.cs
+++ Carsales.Data/Migrations/Configuration.cs
@@ -14,6 +14,72 @@ namespace Carsales.Data.Migrations
 
         protected override void Seed(Carsales.Data.DbConnections.CarSalesDb context)
         {
+            context.Cars.AddOrUpdate(c => c,
+                new Car[]
+                {
+                    new Car()
+                    {
+                        Comments = "",
+                        ContactName = "Andy",
+                        DapPrice = 73000,
+                        DealerAbn = "80 152 673 936",
+                        EgcPrice = 0,
+                        Email = "abc@123.com",
+                        Id = 1,
+                        Make = "Mercedes Benz",
+                        Model = "C200 Coupe",
+                        Phone = "0409 123 456",
+                        Year = "2016",
+                        PriceType = CarPriceType.DAP
+                    },
+                    new Car
+                    {
+                        Comments = "",
+                        ContactName = "John",
+                        DapPrice = 43000,
+                        DealerAbn = "80 152 673 936",
+                        EgcPrice = 0,
+                        Email = "abc@123.com",
+                        Id = 2,
+                        Make = "Toyota",
+                        Model = "86 Blackline",
+                        Phone = "0409 123 456",
+                        Year = "2015",
+                        PriceType = CarPriceType.DAP
+                    },
+                    new Car
+                    {
+                        Comments = "",
+                        ContactName = "Jane",
+                        DapPrice = 45000,
+                        DealerAbn = "80 152 673 936",
+                        EgcPrice = 0,
+                        Email = "abc@123.com",
+                        Id = 3,
+                        Make = "Subaru",
+                        Model = "Liberty 3.6R",
+                        Phone = "0409 123 456",
+                        Year = "2016",
+                        PriceType = CarPriceType.DAP
+                    },
+                    new Car
+                    {
+                        Comments = "",
+                        ContactName = "Steve",
+                        DapPrice = 52000,
+                        DealerAbn = "80 152 673 936",
+                        EgcPrice = 0,
+                        Email = "abc@123.com",
+                        Id = 3,
+                        Make = "Ford",
+                        Model = "Mustang EcoBoost",
+                        Phone = "0409 123 456",
+                        Year = "2016",
+                        PriceType = CarPriceType.DAP
+                    }
+                }
+                );
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
