89d700345ca91df83346963cf80403455f70bfd5
atsq <atsq17@gmail.com>
2016-04-26 15:47:46 +1000

    Enabled CodeFirst Migrations

---
+++ Carsales.Data/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Carsales.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Carsales.Data.DbConnections.CarSalesDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Carsales.Data.DbConnections.CarSalesDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
